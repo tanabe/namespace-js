847c126a2ba8c297abcdb7238ccccf8f85499e60
tanabe <tanablog@gmail.com>
2010-12-13 19:08:21 +0900

    add sample, update readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 #Simple Namespace Library
+---
+##usage
+    //define namespace
+    var module = Namespace("com.kaihatsubu.utils");
+    console.log(module === com.kaihatsubu.utils);//true
+
+    //define method
+    com.kaihatsubu.utils.arrayUtil = {
+      shuffle: function() {
+        console.log("shuffle!");
+      }
+    };
+    com.kaihatsubu.utils.arrayUtil.shuffle();
+    //shuffle!
+
+    //prevent from redefine
+    Namespace("com.kaihatsubu.utils");
+    com.kaihatsubu.utils.arrayUtil.shuffle();
+    //shuffle!
+    
---
+++ sample/namespace.js
@@ -0,0 +1,29 @@
+/**
+ *  Simple Namespace Library
+ *  define a namespace
+ *  @author Hideaki Tanabe <http://blog.kaihatsubu.com>
+ *
+ *  USAGE:
+ *  var module = Namespace("com.kaihatsubu.utils");
+ *  module === com.kaihatsubu.utils; //true
+ *  com.kaihatsubu.utils.arrayUtil = {
+ *    //...
+ *  };
+ *  
+ *  @param ns namespace string
+ *  @return defined namespace
+ */
+var Namespace = function(ns) {
+  var parts = ns.split(".");
+  var parent = window[parts[0]] || function() {
+    return window[parts[0]] = {};
+  }();
+  parts = parts.slice(1);
+  for (var i = 0, length = parts.length; i < length; i++) {
+    if (typeof parent[parts[i]] === "undefined") {
+      parent[parts[i]] = {};
+    }
+    parent = parent[parts[i]];
+  }
+  return parent;
+}
